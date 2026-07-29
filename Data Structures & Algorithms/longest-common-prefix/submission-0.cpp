fc22417c95230e042f7f3c69747e8608b2234a20
Bojja Siva Sai Prasanna Rameswari <147472476+prasannnna@users.noreply.github.com>
2026-07-29 12:17:13 +0530

    Add: longest-common-prefix - submission-0

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string prefix = strs[0];
+        for(string s : strs) {
+            while(s.find(prefix) != 0) {
+                prefix.pop_back();
+
+                if(prefix.empty()) {
+                    return "";
+                }
+            }
+        }
+        return prefix;
+    }
+};
