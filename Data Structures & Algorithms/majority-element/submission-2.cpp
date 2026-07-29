5c1b32ac21a5687cc8b20dcc86dcdc0134999c64
Bojja Siva Sai Prasanna Rameswari <147472476+prasannnna@users.noreply.github.com>
2026-07-29 14:33:53 +0530

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int ans = 0, cnt = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            if(cnt == 0) {
+                ans = nums[i];
+            }
+            if(nums[i] == ans) {
+                cnt++;
+            }
+            else {
+                cnt--;
+            }
+        }
+        return ans;
+    }
+};
